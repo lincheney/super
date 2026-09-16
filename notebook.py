eada927bff99765bb967ad497c0d6bcb3f26f405
林千里 <lincheney@gmail.com>
2026-09-17 01:35:44 +1000

    go backwards

--- notebook.py
+++ notebook.py
@@ -50,7 +50,7 @@ def parse_hostplus(datetime, hostplus_raw, itertools, mo):
         'name': f'hostplus-{_x['OptionName']}',
         'value': 1+_x['Return']/100 if _x['Return'] is not None else None,
         'return': _x['Return'],
-        'date': datetime.date.strptime(_x['MonthEndDate'].split('T')[0], '%Y-%m-%d'),
+        'date': datetime.datetime.strptime(_x['MonthEndDate'].split('T')[0], '%Y-%m-%d'),
 
     } for _x in hostplus_raw['msg']['MonthlyOptions']]
 
@@ -78,7 +78,7 @@ def parse_aussuper(aussuper_raw, datetime, mo):
         _year = _row['Financial Year']
         if not _year.isdigit():
             continue
-        _date = datetime.date(int(_year), 6, 30)
+        _date = datetime.datetime(int(_year), 6, 30)
 
         for _k, _v in _row.items():
             if _k != 'Financial Year':
@@ -93,59 +93,63 @@ def parse_aussuper(aussuper_raw, datetime, mo):
 
 
 @app.cell
-def make_alldata(aussuper, hostplus, itertools, starting_balance):
-    alldata = sorted(hostplus + aussuper, key=lambda x: (x['name'], x['date']))
+def make_alldata(aussuper, datetime, ending_balance, hostplus, itertools):
+    alldata = sorted(hostplus + aussuper, key=lambda x: (x['name'], -x['date'].timestamp()))
 
     for _key, _group in itertools.groupby(alldata, key=lambda x: x['name']):
         _group = [x for x in _group if x['value'] is not None]
 
-        _balance = starting_balance.value
-        # $78 / year
+        _balance = ending_balance.value
         for _k, _g in itertools.groupby(_group, key=lambda x: x['date'].year + (1 if x['date'].month >= 7 else 0)):
             _g = list(_g)
             _aussuper_asset_fee = 0
             for _x in _g:
-                _balance *= _x['value']
                 if _key.startswith('hostplus'):
                     # $78 / year
-                    _balance -= 78 / len(_g)
+                    _balance += 78 / len(_g)
                 elif _key.startswith('aussuper'):
                     # $52 / year + min(600, 0.12%)
                     _fee = min(0.12/100 * _balance, 600 - _aussuper_asset_fee)
-                    _balance -= 52 / len(_g) + _fee
+                    _balance += 52 / len(_g) + _fee
                     _aussuper_asset_fee += _fee
                 else:
                     raise NotImplementedError(_key)
+                _balance /= _x['value']
                 _x['balance'] = _balance
 
         for _x in _group:
-            _x['rev_balance'] = _balance - _x['balance']
+            _x['rev_balance'] = ending_balance.value - _x['balance']
+
+    alldata.extend({
+        'name': name,
+        'balance': ending_balance.value,
+        'rev_balance': 0,
+        'date': datetime.datetime(2027, 7, 1),
+    } for name in set(x['name'] for x in alldata))
     return (alldata,)
 
 
 @app.cell
 def filter_rev_cumproduct(aussuper, hostplus, mo):
-    starting_balance = mo.ui.number(start=100_000, label="Starting balance")
+    ending_balance = mo.ui.number(start=100_000, label="Ending balance")
     _options = sorted(set(x['name'] for x in hostplus + aussuper))
     multiselect = mo.ui.multiselect(options=_options, label='Filter')
-    mo.vstack([starting_balance, multiselect])
-    return multiselect, starting_balance
+    mo.vstack([ending_balance, multiselect])
+    return ending_balance, multiselect
 
 
 @app.cell(hide_code=True)
 def rev_cumproduct_graph(alldata, alt, mo, multiselect):
-    _selection = alt.selection_point(fields=['name'], bind='legend')
     _data = [x for x in alldata if not multiselect.value or x['name'] in multiselect.value]
     chart = (
         alt.Chart(alt.InlineData(_data))
         .mark_line()
         .encode(
-            x=alt.X("date:T", scale=alt.Scale(reverse=False)),
-            y='balance:Q',
+            x=alt.X("date:T", scale=alt.Scale(reverse=True)),
+            y='rev_balance:Q',
             color='name:N',
-            strokeOpacity=alt.when(_selection).then(alt.value(0.8)).otherwise(alt.value(0.2)),
         )
-        .add_params(_selection)
+        .properties(width="container")
     )
     mo.ui.altair_chart(chart)
     return
