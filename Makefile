1af5fc88601135fb0be001faaf79676192cc0494
林千里 <lincheney@gmail.com>
2026-09-17 18:28:50 +1000

    use aussuper daily rates

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@
 .SHELLFLAGS = -eu -o pipefail -x -c
 .SILENT:
 
+FIREFOX = Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:155.0) Gecko/20100101 Firefox/155.0
+
 all: hostplus australiansuper ;
 
 hostplus: public/hostplus.json ;
@@ -10,9 +12,11 @@ public/hostplus.json:
 	auth_token="$$(curl https://hostplus.com.au/content/hostplus-program/home/members/our-products-and-services/investment-options/investment-returns.irm.auth.json | jq -re .)"
 	curl 'https://hostplus.com.au/content/hostplus-program/home/members/our-products-and-services/investment-options/investment-returns.irm.returnscompare.json?InvestmentProduct=13&frequencyType=2&Startdate=1980-01-01&Enddate=2026-09-16&OptionsToCompare=ALL' --compressed -H 'Content-Type: application/json' -H "IRM-Authorization: Bearer $$auth_token" --compressed --fail -o $@
 
-australiansuper: public/australiansuper.csv ;
+australiansuper: public/australiansuper.csv public/australiansuper-daily.csv ;
 public/australiansuper.csv:
-	curl 'https://www.australiansuper.com/api/graphs/annualrates/graph/download/super' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:155.0) Gecko/20100101 Firefox/155.0' --compressed --fail -o $@
+	curl 'https://www.australiansuper.com/api/graphs/annualrates/graph/download/super' -H 'User-Agent: ${FIREFOX}' --compressed --fail -o $@
+public/australiansuper-daily.csv:
+	curl 'https://www.australiansuper.com//api/graphs/dailyrates/download/?start=01/07/2008&end=15/09/2026&cumulative=False&superType=super&truncateDecimalPlaces=True&outputFilename=Daily%20Rates%2001%20Jul%202008%20-%2015%20Sep%202026.csv' -H 'User-Agent: ${FIREFOX}' --compressed --fail -o $@
 
 unisuper: public/unisuper/ ;
 public/unisuper/:
--- notebook.py
+++ notebook.py
@@ -12,8 +12,9 @@ def _():
     import csv
     import datetime
     import itertools
+    import re
 
-    return alt, csv, datetime, itertools, json, mo
+    return alt, csv, datetime, itertools, json, mo, re
 
 
 @app.function
@@ -27,12 +28,15 @@ def read_file(path):
             return file.read()
 
 
-@app.cell
-def _(itertools):
-    def cumproduct(values):
-        return itertools.accumulate(values, lambda x, y: x * y)
+@app.function
+def fy_of_date(date):
+    return date.year + (1 if date.month >= 7 else 0)
 
-    return
+
+@app.function
+def cumproduct(values):
+    import itertools
+    return itertools.accumulate(values, lambda x, y: x * y)
 
 
 @app.cell
@@ -67,51 +71,83 @@ def parse_hostplus(datetime, hostplus_raw, itertools, mo):
 @app.cell
 def load_aussuper(csv, mo):
     _file = mo.notebook_location()/'public'/'australiansuper.csv'
-    aussuper_raw = list(csv.DictReader(read_file(_file).decode('latin1').splitlines()))
-    return (aussuper_raw,)
+    aussuper_annual_raw = list(csv.DictReader(read_file(_file).decode('latin1').splitlines()))
+    _file = mo.notebook_location()/'public'/'australiansuper-daily.csv'
+    aussuper_daily_raw = list(csv.DictReader(read_file(_file).decode().splitlines()))
+    return aussuper_annual_raw, aussuper_daily_raw
 
 
 @app.cell
-def parse_aussuper(aussuper_raw, datetime, mo):
+def parse_aussuper(
+    aussuper_annual_raw,
+    aussuper_daily_raw,
+    datetime,
+    itertools,
+    mo,
+    re,
+):
     aussuper = []
-    for _row in aussuper_raw:
+
+    _daily_names = aussuper_daily_raw[0].keys() - {'Rate Date'}
+    _min_daily_fy = {name: fy_of_date(datetime.datetime.strptime(min(x['Rate Date'] for x in aussuper_daily_raw if x[name]), '%Y-%m-%d') - datetime.timedelta(days=1)) + 1 for name in _daily_names}
+    print(_min_daily_fy)
+
+    for _row in aussuper_annual_raw:
         _year = _row['Financial Year']
-        if not _year.isdigit():
-            continue
-        _date = datetime.datetime(int(_year), 6, 30)
+        _year = int(_year) if _year.isdigit() else 9999
+        _date = datetime.datetime(_year, 6, 30)
 
         for _k, _v in _row.items():
-            if _k != 'Financial Year':
+            _k = re.sub(r'[^\w\s]', '', _k)
+            if _k != 'Financial Year' and _year < _min_daily_fy.get(_k, 9999):
                 aussuper.append({
                     'fund': 'aussuper',
                     'name': f'aussuper-{_k}',
                     'value': 1+float(_v.strip('%'))/100 if _v else None,
                     'date': _date,
                 })
+
+    # daily is too fine grained and causes too much data, turn it down
+    for _month, _group in itertools.groupby(sorted(aussuper_daily_raw, key=lambda x: x['Rate Date']), key=lambda x: x['Rate Date'].rpartition('-')[0]):
+        _group = list(_group)
+        _date = datetime.datetime.strptime(_group[0]['Rate Date'], '%Y-%m-%d')
+        _fy = fy_of_date(_date)
+        for _k in _daily_names:
+            _value = list(cumproduct(1+float(x[_k] or 0)/100 for x in _group))[-1]
+            aussuper.append({
+                'fund': 'aussuper',
+                'name': f'aussuper-{_k}',
+                'value': _value,
+                'date': _date,
+            })
+
     mo.ui.table(aussuper)
     return (aussuper,)
 
 
-@app.cell
-def make_alldata(aussuper, datetime, ending_balance, hostplus, itertools):
-    alldata = sorted(hostplus + aussuper, key=lambda x: (x['name'], -x['date'].timestamp()))
+@app.function
+def make_alldata(*args, ending_balance):
+    import itertools
+    import datetime
+
+    alldata = sorted(itertools.chain.from_iterable(args), key=lambda x: (x['name'], -x['date'].timestamp()))
 
     for _key, _group in itertools.groupby(alldata, key=lambda x: x['name']):
         _group = [x for x in _group if x['value'] is not None]
 
         _balance = ending_balance.value
-        for _k, _g in itertools.groupby(_group, key=lambda x: x['date'].year + (1 if x['date'].month >= 7 else 0)):
+        for _k, _g in itertools.groupby(_group, key=lambda x: fy_of_date(x['date'])):
             _g = list(_g)
-            _aussuper_asset_fee = 0
+            _asset_fee = 0
             for _x in _g:
                 if _key.startswith('hostplus'):
-                    # $78 / year
+                    # $78 per year
                     _balance += 78 / len(_g)
                 elif _key.startswith('aussuper'):
-                    # $52 / year + min(600, 0.12%)
-                    _fee = min(0.12/100 * _balance, 600 - _aussuper_asset_fee)
+                    # $52 + min(600, 0.12%) per year
+                    _fee = min(0.12/100 * _balance / len(_g), 600 - _asset_fee)
                     _balance += 52 / len(_g) + _fee
-                    _aussuper_asset_fee += _fee
+                    _asset_fee += _fee
                 else:
                     raise NotImplementedError(_key)
                 _balance /= _x['value']
@@ -126,12 +162,12 @@ def make_alldata(aussuper, datetime, ending_balance, hostplus, itertools):
         'rev_balance': 0,
         'date': datetime.datetime(2027, 7, 1),
     } for name in set(x['name'] for x in alldata))
-    return (alldata,)
+    return alldata
 
 
 @app.cell
 def filter_rev_cumproduct(aussuper, hostplus, mo):
-    ending_balance = mo.ui.number(start=100_000, label="Ending balance")
+    ending_balance = mo.ui.number(start=1, value=100_000, label="Ending balance")
     _options = sorted(set(x['name'] for x in hostplus + aussuper))
     multiselect = mo.ui.multiselect(options=_options, label='Filter')
     mo.vstack([ending_balance, multiselect])
@@ -139,8 +175,15 @@ def filter_rev_cumproduct(aussuper, hostplus, mo):
 
 
 @app.cell(hide_code=True)
-def rev_cumproduct_graph(alldata, alt, mo, multiselect):
-    _data = [x for x in alldata if not multiselect.value or x['name'] in multiselect.value]
+def rev_cumproduct_graph(
+    alt,
+    aussuper,
+    ending_balance,
+    hostplus,
+    mo,
+    multiselect,
+):
+    _data = [x for x in make_alldata(aussuper, hostplus, ending_balance=ending_balance) if not multiselect.value or x['name'] in multiselect.value]
     chart = (
         alt.Chart(alt.InlineData(_data))
         .mark_line()
